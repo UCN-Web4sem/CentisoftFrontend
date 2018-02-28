33b47e1195073864d7b2b5c45441059a8b99f66c
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2018-02-28 11:02:22 +0100

    Added scafolding for rendering of page items

--- main.js
+++ main.js
@@ -9,16 +9,23 @@ $(document).ready(function () {
 
 	var pageItems = ["developer", "project", "task", "customer", "client"];
 
+	function renderPageItem(pageItem) {
+		return () => {
+			console.log(pageItem);
+		};
+	}
+
 	function buildNavBar() {
-		var template = document.getElementById("nav-item-template");
+		var template = document.querySelector("#nav-item-template");
+		var parent = template.parentElement;
 		var content = template.content;
 
 		pageItems.forEach(e => {
 			var clone = content.cloneNode(true);
 			var a = clone.querySelector("a");
 			a.innerText = e;
-			template.parentNode.appendChild(clone);
-			// a.onclick = TODO:
+			parent.appendChild(clone);
+			a.onclick = renderPageItem(e);
 		});
 	}
 
