2e1622bc6b6fc4729a3dd0d11d518f649b388069
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2018-02-28 12:06:32 +0100

    Auto generate the datapoint table header

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+	var apiURL = "http://dm.sof60.dk:84/api/";
+
 	var map = (l, f) => {
 		var ret = [];
 		for (let i = 0; i < l.length; i++) {
@@ -10,8 +12,55 @@ $(document).ready(function () {
 	var pageItems = ["developer", "project", "task", "customer", "client"];
 
 	function renderPageItem(pageItem) {
+		function getAllDevelopers(cb) {
+			$.get(apiURL+pageItem, (data) => {
+				cb(null, data);
+			}).fail((err) => {
+				cb(err, null);
+			});
+		}
+
+		function renderDataView(datapoints) {
+			if (datapoints.length < 1) return;
+
+			var headerTemplate = document.getElementById("table-header-template");
+			var headerParent = headerTemplate.parentElement;
+			var headerContent = headerTemplate.content;
+
+			var keys = Object.keys(datapoints[0]);
+			keys.forEach(key => {
+				var clone = headerContent.cloneNode(true);
+				clone.querySelector("th").innerText = key;
+				headerParent.appendChild(clone);
+			});
+
+
+			var rowTemplate = document.getElementById("table-row-template");
+			var rowParent = rowTemplate.parentElement;
+			var rowContent = rowTemplate.content;
+
+			
+		}
+
 		return () => {
+			// Clear the templates
+			document.getElementById("table-header-template").parentElement.innerHTML = document.getElementById("table-header-template").outerHTML;
+			document.getElementById("table-row-template").parentElement.innerHTML = document.getElementById("table-row-template").outerHTML;
+
 			console.log(pageItem);
+			switch (pageItem) {
+				case "developer":
+					getAllDevelopers((err, data) => {
+						if (err != null) console.log(err);
+						renderDataView(data);
+						
+					})
+					break;
+			
+				default:
+					break;
+			}
+
 		};
 	}
 
