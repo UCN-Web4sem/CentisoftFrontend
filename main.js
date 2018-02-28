cbf79bd40efe69f4f5f6f682458a6f9661bd571e
Jesper Broge Jørgensen <jesperbroge@gmail.com>
2018-02-28 10:37:55 +0100

    auto generate navbar items from js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-	console.log("Hello, World!");
+	var map = (l, f) => {
+		var ret = [];
+		for (let i = 0; i < l.length; i++) {
+			ret.push(f(l[i]));
+		}
+		return ret
+	}
+
+	var pageItems = ["developer", "project", "task", "customer", "client"];
+
+	function buildNavBar() {
+		var template = document.getElementById("nav-item-template");
+		var content = template.content;
+
+		pageItems.forEach(e => {
+			var clone = content.cloneNode(true);
+			var a = clone.querySelector("a");
+			a.innerText = e;
+			template.parentNode.appendChild(clone);
+			// a.onclick = TODO:
+		});
+	}
+
+	buildNavBar();
 });
